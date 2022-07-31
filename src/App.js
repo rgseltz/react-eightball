cdc243a0b3768bad1541918417a7e94818aae197
ryan <ryanjgecelter@gmail.com>
2022-07-31 17:48:17 +0300

    Submit raw final

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import logo from './logo.svg';
 import './App.css';
 import EightBall from './EightBall';
 import answers from './answers';
+import './EightBall.css';
 
 const App = () => (
 	<div className="App">
--- src/EightBall.js
+++ src/EightBall.js
@@ -1,9 +1,27 @@
 import React, { useState } from 'react';
 
 const EightBall = ({ answers }) => {
+	const ranAnswer = () => {
+		let ranIndx = Math.floor(Math.random() * answers.length);
+		return answers[ranIndx];
+	};
+	const [ answer, setAnswer ] = useState(null);
+	const color = {
+		positive: 'green',
+		uncertain: 'goldenrod',
+		negative: 'red'
+	};
+
+	ranAnswer();
+	console.log(ranAnswer());
 	return (
-		<div>
-			<h1>Eightball</h1>
+		<div className="EightBall" style={{ backgroundColor: answer === null ? 'black' : answer.color }}>
+			<h1 className="EightBall-header">Eightball</h1>
+			<p className="EightBall-instructions">Click To Shake</p>
+			<button className="EightBall-answer-button" onClick={() => setAnswer(ranAnswer)}>
+				Your Answer Awaits
+			</button>
+			{answer === null ? null : <h2>{answer.msg}</h2>}
 		</div>
 	);
 };
